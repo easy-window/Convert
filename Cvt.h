6808153811b804f6a907702f0c1aea27fb4b0829
hancel.lin <imlinhanchao@foxmail.com>
2020-06-01 14:34:00 +0800

    first commit

---
+++ Cvt.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace Easy {
+
+class Cvt
+{
+public:
+	static COleDateTime ToDateTime(CString sDateTime);
+	static long ToNumber(CString sNumber, int nRadix);
+	static double ToNumber(CString sNumber);
+
+	static CString ToString(bool bValue, CString sTrue=_T("1"), CString sFalse=_T("0"));
+	static CString ToString(int nValue, bool bHex=false);	
+	static CString ToString(long nValue, bool bHex=false);
+	static CString ToString(UINT nValue, bool bHex=false);
+	static CString ToString(DWORD dwValue, bool bHex=false);
+	static CString ToString(float fValue, int nPrecision=0);
+	static CString ToString(double fValue, int nPrecision=0);	
+	static CString ToString(COleDateTime date);
+	static CString ToString( const wchar_t* pszFormat, ... );
+};
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Type Conversion Libraries
+A different type conversion libraries.
+
+## Usage
+
+```cpp
+#include "Cvt.h"
+using namespace Easy;
+
+Cvt::ToString(1);               // "1"
+Cvt::ToString(15);              // "f"
+Cvt::ToString(3.1415926, 2);    // "3.14"
+Cvt::ToNumber(_T("3.14"));      // 3.1400000
+```
